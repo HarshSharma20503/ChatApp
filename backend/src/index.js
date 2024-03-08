68842db92731acdc5ba68b22732ec2331297e025
Harsh Sharma <harshsharma20503@gmail.com>
2024-03-08 15:02:59 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import connectDB from "./config/connectDB.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    console.log("Database connected");
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Welcome to Home route" });
+});
